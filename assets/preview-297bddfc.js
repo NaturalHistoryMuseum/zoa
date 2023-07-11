dcc3768a87b94504b6b7b568cc0a533956c5d6fa
alycejenni <alycejenni@users.noreply.github.com>
2023-07-11 16:38:23 +0000

    deploy: 69121f197a0b00234154df5c328c3250a635cf2c

---
+++ assets/preview-297bddfc.js
@@ -0,0 +1,2 @@
+const o={parameters:{backgrounds:{default:"light"},actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{o as default};
+//# sourceMappingURL=preview-297bddfc.js.map
