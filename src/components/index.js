de7e014f59574d1b83266877aecba6da26e16aa6
Alice Butcher <alycejenni@gmail.com>
2023-08-16 13:03:53 +0100

    feat: add dropdown

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as ZoaButton } from './inputs/Button.vue';
 export { default as ZoaTextbox } from './inputs/Textbox.vue';
+export { default as ZoaDropdown } from './inputs/Dropdown.vue';
+
 export { default as ZoaFlash } from './dialogs/Flash.vue';
+export { default as ZoaModal } from './dialogs/Modal.vue';
---
+++ src/components/inputs/Dropdown.vue
@@ -0,0 +1,81 @@
+<template>
+  <div
+    :class="[$style.grid, $style[`grid--${labelPosition}`]]"
+    :id="componentId"
+  >
+    <label
+      :for="subId('dropdown')"
+      v-if="label && labelPosition !== 'none'"
+      :class="[$style.label, $style[`label--${labelPosition}`]]"
+    >
+      {{ label }}
+    </label>
+    <div :class="$style.wrapper">
+      <select :id="subId('dropdown')" :class="$style.input">
+        <option v-for="opt in dropdownOptions" value="opt.value">
+          {{ opt.label }}
+        </option>
+      </select>
+      <font-awesome-icon icon="fa-solid fa-caret-down" :class="$style.arrow" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { commonProps } from './common.js';
+import { getProps } from '../utils/props.js';
+import { useComponentId } from '../utils/compid.js';
+import { computed } from 'vue';
+import FontAwesomeIcon from '../../icons.js';
+
+const props = defineProps(
+  getProps(commonProps, {
+    include: ['labelPosition', 'label'],
+    additional: {
+      options: {
+        type: Array,
+      },
+    },
+  }),
+);
+
+const { componentId, subId } = useComponentId();
+
+const dropdownOptions = computed(() => {
+  let outputOptions = [];
+  props.options.forEach((o) => {
+    if (typeof o === 'object') {
+      outputOptions.push({ label: o.label, value: o.value });
+    } else {
+      outputOptions.push({ label: o, value: o });
+    }
+  });
+  return outputOptions;
+});
+</script>
+
+<style module lang="scss">
+@import './inputs';
+
+.input {
+  appearance: none;
+  -webkit-appearance: none;
+  width: 100%;
+  cursor: pointer;
+}
+
+.wrapper {
+  position: relative;
+
+  & > .arrow {
+    position: absolute;
+    top: 0;
+    right: 1em;
+    bottom: 0;
+    margin: auto;
+    padding-left: 1em;
+    pointer-events: none;
+    cursor: pointer;
+  }
+}
+</style>
