5e7bcd4e867746877736f6c5574c95f071823bd4
Ginger Butcher <alycejenni@gmail.com>
2023-08-17 10:04:29 +0100

    feat: add checkbox

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as ZoaButton } from './inputs/Button.vue';
 export { default as ZoaTextbox } from './inputs/Textbox.vue';
 export { default as ZoaDropdown } from './inputs/Dropdown.vue';
+export { default as ZoaCheckbox } from './inputs/Checkbox.vue';
 
 export { default as ZoaFlash } from './dialogs/Flash.vue';
 export { default as ZoaModal } from './dialogs/Modal.vue';
---
+++ src/components/inputs/Checkbox.vue
@@ -0,0 +1,102 @@
+<template>
+  <label
+    :id="componentId"
+    :for="subId('checkbox')"
+    :class="[$style.grid, $style[`grid--${labelPosition}`]]"
+  >
+    <span
+      v-if="label && labelPosition !== 'none'"
+      :class="[$style.label, $style[`label--${labelPosition}`]]"
+    >
+      {{ label }}
+    </span>
+    <input
+      type="checkbox"
+      :id="subId('checkbox')"
+      :class="$style.defaultCheckbox"
+      v-model="checked"
+      @change="emit('change')"
+    />
+    <span :class="$style.checkbox">
+      <font-awesome-icon icon="fa-solid fa-check" :class="$style.check" />
+    </span>
+  </label>
+</template>
+
+<script setup>
+import { commonProps } from './common.js';
+import { getProps } from '../utils/props.js';
+import { useComponentId } from '../utils/compid.js';
+import { ref } from 'vue';
+import FontAwesomeIcon from '../../icons.js';
+
+const props = defineProps(
+  getProps(commonProps, {
+    include: ['labelPosition', 'label', 'placeholder'],
+    defaults: { labelPosition: 'left' },
+  }),
+);
+
+const { componentId, subId } = useComponentId();
+
+const checked = ref(null);
+
+const emit = defineEmits(['change']);
+</script>
+
+<style module lang="scss">
+@import 'inputs';
+
+.grid {
+  width: min-content;
+  justify-items: center;
+  &.grid--left,
+  &.grid--right {
+    grid-template-columns: min-content min-content;
+  }
+}
+
+.defaultCheckbox {
+  display: none;
+}
+
+.label {
+  cursor: pointer;
+}
+
+.checkbox {
+  height: 1.5em;
+  width: 1.5em;
+  cursor: pointer;
+  background: white;
+  border: 1px solid $grey;
+  border-radius: $rounding;
+  position: relative;
+
+  .defaultCheckbox:checked ~ & {
+    background: $primary-a;
+
+    & > .check {
+      display: block;
+    }
+  }
+
+  .grid--left > & {
+    margin-right: $h-pad;
+  }
+
+  .grid--right > & {
+    margin-left: $h-pad;
+  }
+}
+
+.check {
+  display: none;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  margin: auto;
+}
+</style>
