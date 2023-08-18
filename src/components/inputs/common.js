34eafe9b8d163dd7cf00cd21f130b2cd8589c5da
Ginger Butcher <alycejenni@gmail.com>
2023-08-18 12:35:09 +0100

    docs: add docs for useChangeEmits

--- src/components/inputs/common.js
+++ src/components/inputs/common.js
@@ -1,18 +1,27 @@
 import { debounce } from 'dettle';
 
 export function useChangeEmits(emit, delay) {
+  /**
+   * Defines common emit functions for when inputs change.
+   * @param emit - The emit object returned by defineEmits in <script setup>.
+   * @param {number} delay - The debounce delay for emitting the 'change' event.
+   */
   try {
     delay = parseInt(delay) || 0;
   } catch {
     delay = 0;
   }
+
   function _emitChange(event) {
     emit('change', event);
   }
+
   const emitChange = debounce(_emitChange, delay);
+
   function valueChanged(event) {
     emit('update:modelValue', event.target.value);
     emitChange(event);
   }
+
   return { emitChange, valueChanged };
 }
