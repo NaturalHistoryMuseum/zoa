2ccaff5619ae8c9a039a20a4b86bebf628ab6871
Alice Butcher <alycejenni@gmail.com>
2023-08-31 10:27:33 +0100

    fix: add sliders to index

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,8 @@ export { default as ZoaCheckbox } from './inputs/checkbox/Checkbox.vue';
 export { default as ZoaNumber } from './inputs/number/Number.vue';
 export { default as ZoaDateSimple } from './inputs/date/DateSimple.vue';
 export { default as ZoaDateAmbiguous } from './inputs/date/DateAmbiguous.vue';
+export { default as ZoaSlider } from './inputs/slider/Slider.vue';
+export { default as ZoaRangeSlider } from './inputs/slider/RangeSlider.vue';
 
 export { default as ZoaFlash } from './dialogs/flash/Flash.vue';
 export { default as ZoaModal } from './dialogs/modal/Modal.vue';
