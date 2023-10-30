ed9fe5033814ef6b9a02a2d0e0a17e36797f6adf
Alice Butcher <alycejenni@gmail.com>
2023-10-30 14:45:26 +0000

    build: add rollup visualiser

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), visualizer()],
   build: {
     lib: {
       entry: {
