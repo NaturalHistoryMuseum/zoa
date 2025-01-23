d35cdd465eeaae3582668ad18069cc891d0bfe39
alycejenni <alycejenni@users.noreply.github.com>
2025-01-23 15:59:35 +0000

    chore: build dist package

--- dist/zoa.js
+++ dist/zoa.js
@@ -1,4 +1,4 @@
-import { Z as s, a as Z, F as t, c as l, M as n, b as u, T } from "./main-Cbo0OEnk.js";
+import { Z as s, a as Z, F as t, c as l, M as n, b as u, T } from "./main-0a5jJdC1.js";
 export {
   s as Zoa,
   Z as ZoaButton,
