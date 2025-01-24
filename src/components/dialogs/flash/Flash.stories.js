d881408ddc08e7ce5de37de7bcc7af6381d96afe
Ginger Burns <alycejenni@gmail.com>
2025-01-24 13:37:51 +0000

    fix(storybook): improve story for flash dialog
    - use renderSetup
    - add args to the component so the controls actually work
    - specify unique code examples for each variant

--- src/components/dialogs/flash/Flash.stories.js
+++ src/components/dialogs/flash/Flash.stories.js
@@ -1,10 +1,11 @@
 import ZoaFlash from './Flash.vue';
+import { renderSetup, htmlArgs } from '../../utils/stories.js';
 
 const template = `
 <zoa-flash :kind="kind"
-           :class="rootClass"
            :header="header"
            :message="message"
+           :class="rootClass"
 />
 `;
 
@@ -33,43 +34,88 @@ const meta = {
 export default meta;
 
 const Base = {
-  args: {},
+  args: {
+    kind: 'info',
+    header: 'Flash dialog header',
+    message:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at tellus at urna condimentum.',
+    class: '',
+  },
   render: (args) => ({
     components: { ZoaFlash },
     setup() {
-      return { args };
+      return renderSetup(args);
     },
     template,
   }),
 };
 
+// this is the main example; if we don't have this then Info is used
+export const Default = {
+  ...Base,
+  tags: ['!dev', '!autodocs'], // hides it from the sidebar and variant section
+};
+
+// VARIANTS =====
+const infoArgs = {
+  kind: 'info',
+};
 export const Info = {
   ...Base,
-  args: {
-    kind: 'info',
+  args: infoArgs,
+  parameters: {
+    docs: {
+      source: {
+        code: `<zoa-flash ${htmlArgs(infoArgs)}/>`,
+      },
+    },
   },
 };
 
+const warningArgs = {
+  kind: 'warning',
+  header: "Are you sure that's a good idea?",
+};
 export const Warning = {
   ...Base,
-  args: {
-    kind: 'warning',
-    header: "Are you sure that's a good idea?",
+  args: warningArgs,
+  parameters: {
+    docs: {
+      source: {
+        code: `<zoa-flash ${htmlArgs(warningArgs)}/>`,
+      },
+    },
   },
 };
 
+const errorArgs = {
+  kind: 'error',
+  header: 'Oh no! Something went wrong.',
+};
 export const Error = {
   ...Base,
-  args: {
-    kind: 'error',
-    header: 'Oh no! Something went wrong.',
+  args: errorArgs,
+  parameters: {
+    docs: {
+      source: {
+        code: `<zoa-flash ${htmlArgs(errorArgs)}/>`,
+      },
+    },
   },
 };
 
+const successArgs = {
+  kind: 'success',
+  header: 'Congratulations! The thing worked.',
+};
 export const Success = {
   ...Base,
-  args: {
-    kind: 'success',
-    header: 'Congratulations! The thing worked.',
+  args: successArgs,
+  parameters: {
+    docs: {
+      source: {
+        code: `<zoa-flash ${htmlArgs(successArgs)}/>`,
+      },
+    },
   },
 };
--- src/components/utils/stories.js
+++ src/components/utils/stories.js
@@ -3,3 +3,11 @@ export function renderSetup(args) {
   delete args.class;
   return args;
 }
+
+export function htmlArgs(argsObj) {
+  // convenience function; just transforms the input args into a string of
+  // HTML-like parameters, e.g. {'kind': 'info'} becomes 'kind="info"'
+  return Object.entries(argsObj)
+    .map((a) => a[0] + '=' + '"' + a[1] + '"')
+    .join(' ');
+}
