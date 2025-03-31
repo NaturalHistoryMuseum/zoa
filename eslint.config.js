78dd486222b6487fa6926950b500ec0e71660a74
Ginger Burns <alycejenni@gmail.com>
2025-03-31 14:47:37 +0100

    ci: add eslint configuration

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  { ignores: ['dist/**/*'] },
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    languageOptions: { globals: globals.browser },
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  pluginVue.configs['flat/recommended'],
+  eslintConfigPrettier,
+]);
